8feacdc7b13447b9a2f26b453dcf7a6e221ce017
Jankefyn <fynn.janke@hs-furtwangen.de>
2023-12-12 15:04:40 +0100

    new js file

--- test/source.js
+++ test/source.js
@@ -1,3 +1,12 @@
-
+window.onload = function() {
+    webgazer.setGazeListener(function(data, elapsedTime) {
+        if (data == null) {
+            return;
+        }
+        var xprediction = data.x; //these x coordinates are relative to the viewport
+        var yprediction = data.y; //these y coordinates are relative to the viewport
+        console.log("x: "+ xprediction); //elapsed time is based on time since begin was called
+    }).begin();
+};
 
 
