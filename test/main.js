6d9d3e7ec78c769bf74503054bd533a141a91884
Jankefyn <fynn.janke@hs-furtwangen.de>
2023-12-13 12:21:00 +0100

    testing if data works like i want

--- test/main.js
+++ test/main.js
@@ -1,20 +1,19 @@
-window.onload = async function() {
+window.onload = async function () {
 
     //start the webgazer tracker
     await webgazer.setRegression('ridge') /* currently must set regression and tracker */
         //.setTracker('clmtrackr')
-        .setGazeListener(function(data, clock) {
-            console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
-          //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
+        .setGazeListener(function (data, clock) {
+            playAudio(data);
         })
         .saveDataAcrossSessions(true)
         .begin();
-        webgazer.showVideoPreview(true) /* shows all video previews */
-            .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
-            .applyKalmanFilter(true); /* Kalman Filter defaults to on. Can be toggled by user. */
+    webgazer.showVideoPreview(true) /* shows all video previews */
+        .showPredictionPoints(true) /* shows a square every 100 milliseconds where current prediction is */
+        .applyKalmanFilter(true); /* Kalman Filter defaults to on. Can be toggled by user. */
 
     //Set up the webgazer video feedback.
-    var setup = function() {
+    var setup = function () {
 
         //Set up the main canvas. The main canvas is used to calibrate the webgazer.
         var canvas = document.getElementById("plotting_canvas");
@@ -26,17 +25,26 @@ window.onload = async function() {
 
 };
 
+function playAudio(data) {
+    if (data.x <= canvas.width/2) {
+        console.log("ich bin links");
+    }
+    else {
+        console.log("ich bin Rechts");
+    }
+}
+
 // Set to true if you want to save the data even if you reload the page.
 window.saveDataAcrossSessions = true;
 
-window.onbeforeunload = function() {
+window.onbeforeunload = function () {
     webgazer.end();
 }
 
 /**
  * Restart the calibration process by clearing the local storage and reseting the calibration point
  */
-function Restart(){
+function Restart() {
     document.getElementById("Accuracy").innerHTML = "<a>Not yet Calibrated</a>";
     webgazer.clearData();
     ClearCalibration();
