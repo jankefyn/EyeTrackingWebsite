c79f0f8a84a0334179ab0e6df8efa007fa4bae46
Jankefyn <fynn.janke@hs-furtwangen.de>
2023-12-13 11:43:50 +0100

    trying to console log the coordinates

--- test/main.js
+++ test/main.js
@@ -4,7 +4,7 @@ window.onload = async function() {
     await webgazer.setRegression('ridge') /* currently must set regression and tracker */
         //.setTracker('clmtrackr')
         .setGazeListener(function(data, clock) {
-          //   console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
+            console.log(data); /* data is an object containing an x and y key which are the x and y prediction coordinates (no bounds limiting) */
           //   console.log(clock); /* elapsed time in milliseconds since webgazer.begin() was called */
         })
         .saveDataAcrossSessions(true)
