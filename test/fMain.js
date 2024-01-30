310ff4b92b6b34cd977b79fdf3da504c236fad31
Jankefyn <fynn.janke@hs-furtwangen.de>
2024-01-30 12:05:27 +0100

    fix unable to decode audio data

--- test/fMain.js
+++ test/fMain.js
@@ -50,7 +50,7 @@ function Restart() {
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 let audioContext = null;
 
-const sounds = ['../test/sounds/amChord.wav', '../test/sounds/dChord.wav', '../test/sounds/fChord.wav', '../test/sounds/gChord.wav',""];
+const sounds = ['../test/sounds/amChord.wav', '../test/sounds/dChord.wav', '../test/sounds/fChord.wav', '../test/sounds/gChord.wav'];
 const levels = [0, 0, 0, 0];
 const loops = [];
 const activeLoops = new Set();
